9e822c26612053610fb0a4d53e2a33c175817f7a
Andrea Amantini <lo.zampino@gmail.com>
2021-11-15 16:13:27 +0100

    Release JS - See notes below 👇
    Note: this includes the SCI context extension with devcards registry and 
    ui functions. We should find a way to extend the static app to
    be mounted on top of an arbitrary js bundle instead.

--- src/nextjournal/clerk/view.clj
+++ src/nextjournal/clerk/view.clj
@@ -92,7 +92,7 @@
 (def resource->static-url
   {"/css/app.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxQBDwk3cvr1bt8YVL5m6bJGrFEmzrSbCrH1roypLjJr4AbbteCKh9Y6gQVYexdY85QA2HG5nQFLWpRp69zFSPDJ9"
    "/css/viewer.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxoxUgsBRs2yjjBBcfeCc8XigM7erXHmjJg2tjdGxNBxwTYuDonuYswXqRStaCA2b3rTEPCgPwixJmAVrea1qAHHU"
-   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxteupedsBexU8wEpyBiahDwp3M7DaezmuMPV49YG3wXGpsj5LSJMBzfMkBfbZAszomgrpEiJKQw2VERCdDsC8yAy"})
+   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VtBfJVxm4oMXkgTpEQJ9AJ8QRRDvBF4QLvmvJ9PkGSai3VcEuWKYNXpt2ynzvp2Z3gAiyBUph2SPAFew7EYS6AB7h"})
 
 (defn ->html [{:keys [conn-ws? live-js?] :or {conn-ws? true live-js? live-js?}} doc]
   (hiccup/html5
